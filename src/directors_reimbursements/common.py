55fc19c612494411f6931eeab32f049587f764c9
Jeff <jeffwatkins2000@gmail.com>
2025-08-17 11:43:07 +0100

    refactor common

--- src/directors_reimbursements/common.py
+++ src/directors_reimbursements/common.py
@@ -4,29 +4,27 @@ from typing import NamedTuple
 from datetime import datetime
 from dateutil import relativedelta
 
-from config import config
+from directors_reimbursements.config import config
 
-date_delta = relativedelta.relativedelta
+DateDelta = relativedelta.relativedelta
 
 
 class Dates(NamedTuple):
+    """Period dates as an object."""
     start_date: datetime.date
     end_date: datetime.date
     payment_date: datetime.date
 
 
 def get_period_dates(today: datetime.date) -> Dates:
+    """Return period dates as a Dates object."""
+    # pylint: disable=no-member)
     payment_month = today.month
     period = config.period_months
     while (payment_month - config.period_start_month) % period != 0:
         payment_month -= 1
 
     payment_date = datetime(today.year, payment_month, 1)
-    end_date = payment_date - date_delta(days=1)
-    start_date = payment_date - date_delta(months=config.period_months)
-    dates = Dates(
-        start_date,
-        end_date,
-        payment_date
-    )
-    return dates
+    end_date = payment_date - DateDelta(days=1)
+    start_date = payment_date - DateDelta(months=config.period_months)
+    return Dates(start_date, end_date, payment_date)
